964c01ef9f022b8b8281046ccf0c7ce72fb4c98a
Pedro Díaz <igoticecream@gmail.com>
2024-03-29 20:31:39 +0100

    reset zoom

--- ZoomControl/ModConfig.cs
+++ ZoomControl/ModConfig.cs
@@ -6,7 +6,9 @@ namespace ZoomControl
     {
         public float ZoomLevel { get; set; } = 1.0f;
         public KeybindList ZoomLevelKey { get; set; } = KeybindList.Parse("LeftShift");
+        public KeybindList ZoomLevelResetKey { get; set; } = KeybindList.Parse("LeftShift+MouseMiddle");
         public float UiScale { get; set; } = 1.0f;
         public KeybindList UiScaleKey { get; set; } = KeybindList.Parse("LeftControl");
+        public KeybindList UiScaleResetKey { get; set; } = KeybindList.Parse("LeftControl+MouseMiddle");
     }
 }
--- ZoomControl/ModEntry.cs
+++ ZoomControl/ModEntry.cs
@@ -1,4 +1,5 @@
 using GenericModConfigMenu;
+using Microsoft.Xna.Framework.Input;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -20,20 +21,9 @@ namespace ZoomControl
 
             // Subscribe to the events
             helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
-            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
             helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
-            helper.Events.GameLoop.Saved += this.OnSaved;
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
             helper.Events.Input.MouseWheelScrolled += this.OnMouseWheelScrolled;
-        }
-
-        private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
-        {
-            if (!Context.IsWorldReady)
-                return;
-
-            // Get current save zoom level and UI scale
-            this.Config.ZoomLevel = Game1.options.zoomLevel;
-            this.Config.UiScale = Game1.options.uiScale;
         }
 
         private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
@@ -52,7 +42,7 @@ namespace ZoomControl
             configMenu?.AddNumberOption(
                 mod: this.ModManifest,
                 () => this.Config.ZoomLevel,
-                (float val) => { this.Config.ZoomLevel = val; this.UpdateZoomLevel(val); },
+                (float val) => { this.Config.ZoomLevel = val; UpdateZoomLevel(this.Config.ZoomLevel); },
                 () => this.Helper.Translation.Get("settings.zoomlevel.name"),
                 () => this.Helper.Translation.Get("settings.zoomlevel.tooltip"),
                 min: Options.minZoom,
@@ -69,10 +59,18 @@ namespace ZoomControl
                 () => this.Helper.Translation.Get("settings.zoomlevelkey.tooltip")
             );
 
+            configMenu?.AddKeybindList(
+                mod: this.ModManifest,
+                () => this.Config.ZoomLevelResetKey,
+                (KeybindList val) => this.Config.ZoomLevelResetKey = val,
+                () => this.Helper.Translation.Get("settings.zoomlevelresetkey.name"),
+                () => this.Helper.Translation.Get("settings.zoomlevelresetkey.tooltip")
+            );
+
             configMenu?.AddNumberOption(
                 mod: this.ModManifest,
                 () => this.Config.UiScale,
-                (float val) => { this.Config.UiScale = val; this.UpdateUIScale(val); },
+                (float val) => { this.Config.UiScale = val; UpdateUIScale(this.Config.UiScale); },
                 () => this.Helper.Translation.Get("settings.uiscale.name"),
                 () => this.Helper.Translation.Get("settings.uiscale.tooltip"),
                 min: Options.minUIZoom,
@@ -88,6 +86,30 @@ namespace ZoomControl
                 () => this.Helper.Translation.Get("settings.uiscalekey.name"),
                 () => this.Helper.Translation.Get("settings.uiscalekey.tooltip")
             );
+
+            configMenu?.AddKeybindList(
+                mod: this.ModManifest,
+                () => this.Config.UiScaleResetKey,
+                (KeybindList val) => this.Config.UiScaleResetKey = val,
+                () => this.Helper.Translation.Get("settings.uiscaleresetkey.name"),
+                () => this.Helper.Translation.Get("settings.uiscaleresetkey.tooltip")
+            );
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            // Ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+            // Ignore if player is not free to act
+            if (!Context.IsPlayerFree)
+                return;
+
+            if (this.Config.ZoomLevelResetKey.JustPressed())
+                UpdateZoomLevel(this.Config.ZoomLevel);
+
+            if (this.Config.UiScaleResetKey.JustPressed())
+                UpdateUIScale(this.Config.UiScale);
         }
 
         private void OnMouseWheelScrolled(object? sender, MouseWheelScrolledEventArgs e)
@@ -99,20 +121,18 @@ namespace ZoomControl
             if (!Context.IsPlayerFree)
                 return;
 
-            //Game1.exitActiveMenu();
-
             if (this.Config.ZoomLevelKey.IsDown())
             {
                 if (e.Delta > 0)
                 {
-                    this.UpdateZoomLevelBy(+INTERVAL);
+                    UpdateZoomLevel((float)Math.Round(Game1.options.zoomLevel + INTERVAL, 2));
 
                     if (!(Game1.player.UsingTool && (Game1.player.CurrentTool == null || !(Game1.player.CurrentTool is FishingRod fishingRod) || (!fishingRod.isReeling && !fishingRod.pullingOutOfWater))))
                         Game1.player.CurrentToolIndex += Game1.options.invertScrollDirection ? -1 : 1;
                 }
                 else if (e.Delta < 0)
                 {
-                    this.UpdateZoomLevelBy(-INTERVAL);
+                    UpdateZoomLevel((float)Math.Round(Game1.options.zoomLevel - INTERVAL, 2));
 
                     if (!(Game1.player.UsingTool && (Game1.player.CurrentTool == null || !(Game1.player.CurrentTool is FishingRod fishingRod) || (!fishingRod.isReeling && !fishingRod.pullingOutOfWater))))
                         Game1.player.CurrentToolIndex += Game1.options.invertScrollDirection ? 1 : -1;
@@ -122,14 +142,14 @@ namespace ZoomControl
             {
                 if (e.Delta > 0)
                 {
-                    this.UpdateUIScaleBy(+INTERVAL);
+                    UpdateUIScale((float)Math.Round(Game1.options.uiScale + INTERVAL, 2));
 
                     if (!(Game1.player.UsingTool && (Game1.player.CurrentTool == null || !(Game1.player.CurrentTool is FishingRod fishingRod) || (!fishingRod.isReeling && !fishingRod.pullingOutOfWater))))
                         Game1.player.CurrentToolIndex += Game1.options.invertScrollDirection ? -1 : 1;
                 }
                 else if (e.Delta < 0)
                 {
-                    this.UpdateUIScaleBy(-INTERVAL);
+                    UpdateUIScale((float)Math.Round(Game1.options.uiScale - INTERVAL, 2));
 
                     if (!(Game1.player.UsingTool && (Game1.player.CurrentTool == null || !(Game1.player.CurrentTool is FishingRod fishingRod) || (!fishingRod.isReeling && !fishingRod.pullingOutOfWater))))
                         Game1.player.CurrentToolIndex += Game1.options.invertScrollDirection ? 1 : -1;
@@ -140,22 +160,17 @@ namespace ZoomControl
         private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
         {
             // Change the zoom level and UI scale when the game is loaded
-            this.UpdateZoomLevel(this.Config.ZoomLevel);
-            this.UpdateUIScale(this.Config.UiScale);
+            UpdateZoomLevel(this.Config.ZoomLevel);
+            UpdateUIScale(this.Config.UiScale);
+
+            // General
+            Game1.options.hideToolHitLocationWhenInMotion = false;
+            Game1.options.pauseWhenOutOfFocus = false;
+            Game1.options.showAdvancedCraftingInformation = true;
+            Game1.options.runButton = new InputButton[1] { new(Keys.LeftControl) };
         }
 
-        private void OnSaved(object? sender, SavedEventArgs e)
-        {
-            // Save configuration
-            this.Helper.WriteConfig(this.Config);
-        }
-
-        private void UpdateZoomLevelBy(float amount)
-        {
-            this.UpdateZoomLevel((float)Math.Round(Game1.options.zoomLevel + amount, 2));
-        }
-
-        private void UpdateZoomLevel(float value)
+        private static void UpdateZoomLevel(float value)
         {
             // Clamp the value within the range of Options.minZoom to Options.maxZoom
             value = (float)Math.Round(Math.Min(Options.maxZoom, Math.Max(Options.minZoom, value)), 2); // or RoundUp?
@@ -165,12 +180,7 @@ namespace ZoomControl
                 Game1.options.desiredBaseZoomLevel = value;
         }
 
-        private void UpdateUIScaleBy(float amount)
-        {
-            this.UpdateUIScale((float)Math.Round(Game1.options.uiScale + amount, 2));
-        }
-
-        private void UpdateUIScale(float value)
+        private static void UpdateUIScale(float value)
         {
             // Clamp the value within the range of Options.minUIZoom to Options.maxUIZoom
             value = (float)Math.Round(Math.Min(Options.maxUIZoom, Math.Max(Options.minUIZoom, value)), 2); // or RoundUp?
